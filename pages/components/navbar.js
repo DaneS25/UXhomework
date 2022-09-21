b65c5e3e7d72eac0cf6f7d11bc7fe7f7a7f763ed
Dane <soredt1@student.op.ac.nz>
2022-09-21 16:46:20 +1200

    failed to build on netlify trying to get it to work

--- pages/components/navbar.js
+++ pages/components/navbar.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import React, { useState } from "react";
-import NavItem from "./navitem";
+import NavItem from "./NavItem";
 
 const MENU_LIST = [
   { text: "Home", href: "/" },
